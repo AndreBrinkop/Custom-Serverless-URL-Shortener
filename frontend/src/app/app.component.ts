7845f6fc05d86f30a835056d44854f7306d5688c
André Brinkop <andre@brinkop.de>
2020-05-21 18:27:01 +0200

    add amplify dependencies and configure amplify to authenticate with cognito

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AmplifyService }  from 'aws-amplify-angular';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'custom-url-shortener-app';
+export class AppComponent implements OnInit {
+  constructor(public amplify: AmplifyService) {
+  }
+
+  ngOnInit(): void {
+    this.amplify.authStateChange$.subscribe(authState => {
+      console.log(authState.user)
+    })
+  }
+
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+import { AmplifyService }  from 'aws-amplify-angular';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+
+/* Configure Amplify resources */
+const cognitoConfig = {
+  "aws_cognito_region": environment.awsCognitoRegion,
+  "aws_user_pools_id": environment.awsUserPoolsId,
+  "aws_user_pools_web_client_id": environment.awsUserPoolsWebClientId
+}
+Amplify.configure(cognitoConfig);
 
 @NgModule({
   declarations: [
@@ -10,9 +23,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AmplifyUIAngularModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AmplifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
